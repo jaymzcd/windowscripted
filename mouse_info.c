dd67c3cf5bf05e1fb35643564b0a104631c325b3
jaymz <jaymz@u-dox.com>
2011-05-17 14:37:37 +0100

    change to poll constantly, show state

--- mouse_info.c
+++ mouse_info.c
@@ -3,26 +3,28 @@
 #include <X11/Xlib.h>
 
 int main() {
-
+    
     Display *dsp = XOpenDisplay(NULL);
     if(!dsp){
         return 1;
     }
     int screenNumber = DefaultScreen(dsp);
-
     XEvent event;
 
-    XQueryPointer(dsp, RootWindow(dsp, DefaultScreen(dsp)),
-        &event.xbutton.root, &event.xbutton.window,
-        &event.xbutton.x_root, &event.xbutton.y_root,
-        &event.xbutton.x, &event.xbutton.y,
-        &event.xbutton.state
-    );
-    
-    printf("Mouse Coordinates: %d %d\n", event.xbutton.x, event.xbutton.y);
+    while(1) {
+        XQueryPointer(dsp, RootWindow(dsp, DefaultScreen(dsp)),
+            &event.xbutton.root, &event.xbutton.window,
+            &event.xbutton.x_root, &event.xbutton.y_root,
+            &event.xbutton.x, &event.xbutton.y,
+            &event.xbutton.state
+        );
+        /* In this case a state val of 16 seems to be clicked. */
+        printf("Mouse Coordinates: %d %d %d\n", event.xbutton.x, event.xbutton.y,
+            event.xbutton.state);
+    }
     
     XCloseDisplay(dsp);
-
+    
     return 0;
 
 }
